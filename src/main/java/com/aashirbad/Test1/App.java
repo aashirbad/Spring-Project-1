6b48405e0a3ea87f25979abda9039b8f4846e6c6
Aashirbad Maharana <aashirbadmaharana11@gmail.com>
2019-10-02 17:48:43 +0530

    some changes

--- src/main/java/com/aashirbad/Test1/App.java
+++ src/main/java/com/aashirbad/Test1/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext app = new ClassPathXmlApplicationContext("spring.xml");
-        Motors obj = (Motors) app.getBean("motor");
+        Motors obj = (Motors) app .getBean("motor");
         obj.drive();
     }
 }
